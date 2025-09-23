9ea1a8823d20249739199293cb8bccd9a19fe0f8
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-23 18:58:36 +0200

    fix the setup lights

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,6 +10,7 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
+      <ambientLight intensity={0.5} />
       <spotLight
         position={[-20, 50, 10]}
         angle={0.12}
@@ -19,6 +20,11 @@ const Computers = () => {
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
+      <directionalLight
+        position={[10, 10, 5]}
+        intensity={1.5}
+        castShadow
+      />
       <primitive
         object={computer.scene}
         scale={ 0.75 }
@@ -53,3 +59,75 @@ const ComputersCanvas = () => {
 };
 
 export default ComputersCanvas;
+
+// import React, { Suspense } from "react";
+// import { Canvas } from "@react-three/fiber";
+// import { OrbitControls, Preload, useGLTF, Environment } from "@react-three/drei";
+
+// const Computers = () => {
+//   const computer = useGLTF("./desktop_pc/scene.gltf");
+
+//   return (
+//     <mesh>
+//       {/* Soft ambient light for base visibility */}
+//       <ambientLight intensity={0.5} />
+
+//       {/* Main spotlight from above */}
+//       <spotLight
+//         position={[-20, 50, 10]}
+//         angle={0.25}
+//         penumbra={1}
+//         intensity={2}
+//         castShadow
+//         shadow-mapSize={1024}
+//       />
+
+//       {/* Fill light from the side */}
+//       <directionalLight
+//         position={[10, 10, 5]}
+//         intensity={1.5}
+//         castShadow
+//       />
+
+//       {/* Small point light near the front */}
+//       <pointLight position={[0, 5, 5]} intensity={1.2} />
+
+//       {/* The actual PC model */}
+//       <primitive
+//         object={computer.scene}
+//         scale={0.75}
+//         position={[0, -3.25, -1.5]}
+//         rotation={[-0.01, -0.2, -0.1]}
+//       />
+//     </mesh>
+//   );
+// };
+
+// const ComputersCanvas = () => {
+//   return (
+//     <Canvas
+//       frameloop='demand'
+//       shadows
+//       dpr={[1, 2]}
+//       camera={{ position: [20, 3, 5], fov: 25 }}
+//       gl={{ preserveDrawingBuffer: true }}
+//     >
+//       <Suspense fallback={null}>
+//         <OrbitControls
+//           enableZoom={false}
+//           maxPolarAngle={Math.PI / 2}
+//           minPolarAngle={Math.PI / 2}
+//         />
+
+//         <Computers />
+
+//         {/* Environment light for realistic reflections */}
+//         <Environment preset="city" />
+//       </Suspense>
+
+//       <Preload all />
+//     </Canvas>
+//   );
+// };
+
+// export default ComputersCanvas;
