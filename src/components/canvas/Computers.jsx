212c270c4391aabd348669c60efed7a1f55eec5d
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-24 08:52:28 +0200

    fix the spacing and some styles

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -28,7 +28,7 @@ const Computers = () => {
       <primitive
         object={computer.scene}
         scale={ 0.75 }
-        position={[0, -4, -1.5]} 
+        position={[0, -3.25, -1.5]} 
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
