db89b69d486e7b205b67cedbdcb3ef387f055ba7
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-24 09:04:09 +0200

    adjust hero indicator position with mobile responsive styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
 
       <ComputersCanvas/>
       
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-2 bottom-4 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
