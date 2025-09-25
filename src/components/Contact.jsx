ed485f216a82d0771cd5048f165a75c354de7e75
yasmine bounasla <yasminebounasla@gmail.com>
2025-09-25 10:11:45 +0200

    fix EmailJS template sender configuration

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,10 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Yasmine Bounasla",
           from_email: form.email,
-          to_email: "yasminebounasla@gmail.com",
           message: form.message,
+          name: form.name
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
